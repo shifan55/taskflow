a28c80bf2a2dadcb0c4cc4c30ecf3a707e29cc2d
Shifan Abdulla <shifanabdulla55@gmail.com>
2026-04-29 17:33:09 +0530

    feat: add tasks API endpoint

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"message": "TaskFlow API is running"}
+@app.get("/tasks")
+def get_tasks():
+    return {"tasks": ["Task 1", "Task 2"]}
